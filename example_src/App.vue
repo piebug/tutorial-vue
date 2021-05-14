31c5b9751eb023d62ce09221dc04dd22d159e0ce
pies-n-loaf <amwright.dev@gmail.com>
2021-05-14 00:35:33 -0500

    Update examples with Vite code

--- example_src/App.vue
+++ example_src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
-<script>
+<script setup>
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
