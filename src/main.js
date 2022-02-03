0ac21374cbcb436ca7443c6aa39fe506b9a13f15
pies-n-loaf <amwright.dev@gmail.com>
2022-02-02 23:36:54 -0600

    Add VueRouter and move employees page to new route

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Employees from './pages/Employees.vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/employees', component: Employees },
+]
+
+const router = createRouter({
+  // createWebHistory is an alternate option
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`
+})
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
