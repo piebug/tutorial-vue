f9d6615392766fe277d0106db7968fbbbcf64a81
pies-n-loaf <amwright.dev@gmail.com>
2022-02-04 12:55:18 -0600

    Restyle EmployeeForm with tailwind css

--- src/components/EmployeeForm.vue
+++ src/components/EmployeeForm.vue
@@ -1,29 +1,39 @@
 <template>
-  <div id="employee-form">
-    <form @submit.prevent="handleSubmit">
-      <label>Employee name</label>
-      <input 
-        ref="first"
-        type="text"
-        :class="{ 'has-error': submitting && invalidName }"
-        v-model="employee.name"
-        @focus="clearStatus"
-        @keypress="clearStatus"
-      />
-      <label>Employee Email</label>
-      <input 
-        type="text"
-        :class="{ 'has-error': submitting && invalidEmail }"
-        v-model="employee.email"
-        @focus="clearStatus"
-      />
+  <section id="employee-form">
+    <form @submit.prevent="handleSubmit" >
+
       <p v-if="error && submitting" class="error-message">
-        Please fill out all required fields
+        Please fill out all required fields.
       </p>
-      <p v-if="success" class="success-message">✅ Employee successfully added</p>
+      <p v-if="success" class="success-message">
+        Employee successfully added!
+      </p>
+
+      <label class="block">
+        <span>New employee name</span>
+        <input 
+          ref="first"
+          type="text"
+          :class="{ 'has-error': submitting && invalidName }"
+          v-model="employee.name"
+          @focus="clearStatus"
+          @keypress="clearStatus"
+        />
+      </label>
+
+      <label class="block">
+        <span>New employee email</span>
+        <input 
+          type="email"
+          :class="{ 'has-error': submitting && invalidEmail }"
+          v-model="employee.email"
+          @focus="clearStatus"
+        />
+      </label>
+
       <button>Add Employee</button>
     </form>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -78,18 +88,38 @@
 
 <style scoped>
   form {
-    margin-bottom: 2rem;
+    @apply grid grid-cols-1 gap-6;
+  }
+
+  label > span {
+    @apply text-sm uppercase font-bold;
+  }
+
+  input[type="text"],
+  input[type="email"] {
+    @apply mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 
+      focus:ring-0 focus:border-sky-500;
+  }
+
+  button {
+    @apply rounded py-2 border-2 border-sky-500 font-bold text-sky-500
+      hover:bg-sky-500 hover:text-white
+      focus:bg-sky-400 focus:text-white;
+  }
+
+  input.has-error {
+    @apply border-b-rose-500;
   }
 
   [class*='-message'] {
-    font-weight: 500;
+    @apply p-3 font-semibold border-l-4;
   }
 
   .error-message {
-    color: #d33c40;
+    @apply text-rose-500 border-l-rose-300;
   }
 
   .success-message {
-    color: #32a95d;
+    @apply text-green-600 border-l-green-200;
   }
 </style>
