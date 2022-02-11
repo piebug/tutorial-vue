45721c7d8ed45d58d3a8e4d55035128c14ff847e
pies-n-loaf <amwright.dev@gmail.com>
2022-02-11 12:21:33 -0600

    Add color preview for converter

--- src/components/ColorConverter.vue
+++ src/components/ColorConverter.vue
@@ -1,7 +1,8 @@
 <template>
   <section id="color-converter">
-    <form>
+    <div class="color-block" :style="colorStyle"></div>
 
+    <form>
       <label class="block">
         <span>HEX</span>
         <hex-input
@@ -40,21 +41,28 @@
         color: {
           hex: '',
           rgb: '',
-        }
+        },
+        colorStyle: {
+          backgroundColor: '',
+        },
       }
     },
     methods: {
       resetRgb() {
         this.color.rgb = ''
+        this.colorStyle.backgroundColor = ''
       },
       resetHex() {
         this.color.hex = ''
+        this.colorStyle.backgroundColor = ''
       },
       async getRgb(hex) {
         try {
           const response = await fetch(`https://www.thecolorapi.com/id?hex=${hex}`)
           const data = await response.json()
+
           this.color.rgb = `${data.rgb.r},${data.rgb.g},${data.rgb.b}`
+          this.colorStyle.backgroundColor = data.hex.value
         } catch (error) {
           console.error(error)
         }
@@ -63,7 +71,9 @@
         try {
           const response = await fetch(`https://www.thecolorapi.com/id?rgb=${rgb}`)
           const data = await response.json()
+
           this.color.hex = data.hex.clean
+          this.colorStyle.backgroundColor = data.hex.value
         } catch (error) {
           console.error(error)
         }
@@ -72,6 +82,27 @@
   }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  #color-converter {
+    .color-block {
+      @apply w-4/5 mx-auto aspect-square rounded-full shadow-md 
+        outline outline-1 outline-gray-200 border-2 border-white;
+      background-color: #FCF6EE;
+      border-color: #FCF6EE;
+    }
 
+    form {
+      @apply grid grid-cols-1 gap-4;
+    }
+
+    button {
+      @apply py-2 rounded-full border-2 border-violet-500;
+    }
+
+    input {
+      @apply mt-1 block w-full rounded-full border-gray-200 shadow-sm 
+        focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50;
+      background-color: #FCF6EE;
+    }
+  }
 </style>
