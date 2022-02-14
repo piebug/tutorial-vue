53e07a677e1861b74a52d4c5ea8ca6a44c63637c
pies-n-loaf <amwright.dev@gmail.com>
2022-02-13 21:29:58 -0600

    Add 404 component

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
+import NotFound from './pages/NotFound.vue'
 import Employees from './pages/Employees.vue'
 import Home from './pages/Home.vue'
 import App from './App.vue'
@@ -8,6 +9,7 @@ import App from './App.vue'
 const routes = [
   { path: '/', component: Home },
   { path: '/employees', component: Employees },
+  { path: '/:any', component: NotFound },
 ]
 
 const router = createRouter({
---
+++ src/pages/NotFound.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="not-found">
+    <main>
+      <h1>404</h1>
+
+      <p class="sub">This page doesn't exist.</p>
+
+      <p>Try one of these instead:</p>
+
+      <nav>
+        <ul>
+          <li class="colors"><router-link to="/">Color Converter</router-link></li>
+          <li class="employees"><router-link to="/employees">Employees</router-link></li>
+        </ul>
+      </nav>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'not-found',
+  }
+</script>
+
+<style scoped lang="scss">
+  #not-found {
+    @apply m-0 p-0 min-h-screen bg-neutral-200 flex font-sans text-center text-lg;
+
+    main {
+      @apply container self-center mx-auto pb-32 lg:w-1/3;
+    }
+
+    h1 {
+      @apply text-9xl font-bold mt-8 mb-4;
+    }
+
+    .sub {
+      @apply text-2xl mb-8;
+    }
+
+    nav {
+      @apply my-4;
+
+      .colors {
+        @apply font-display lowercase text-emerald-500 text-xl mb-1;
+
+        a {
+          @apply hover:underline hover:text-emerald-600;
+        }
+      }
+
+      .employees {
+        @apply font-serif text-stone-600;
+
+        a {
+          @apply hover:underline hover:text-stone-500;
+        }
+      }
+    }
+  }
+</style>
